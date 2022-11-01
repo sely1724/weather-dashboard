434b5a2493e66b37a903f89ac2d3f2600fa16897
Sylvia Ely <sylvianne9417@gmail.com>
2022-11-01 16:53:37 -0700

    verify city entered

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,64 @@
 //search suggestions
 //var citySearched = "input"
 var myKey = "9ee067cb6c60d1cf1d72062b348c22a8"
+var inputEl = $("input");
 
 
 
 
 $("button").on("click", function(event){
     event.preventDefault();
-    console.log("Hi Sylvia");
+    var citySearched = inputEl.val();
+    verifyCityEntered(citySearched);
 })
+
+
+function verifyCityEntered(citySearched){
+    var citySearchedURL = "https://api.openweathermap.org/data/2.5/forecast?q="+citySearched+"&appid="+myKey
+    console.log(citySearchedURL)
+    fetch(citySearchedURL)
+        .then(function(response) {
+        console.log(response);
+        if (response.status === 200) {
+        console.log("OK!")
+        }
+        else{
+        alert("Please enter a valid city name")
+        }
+        return response.json();
+        });
+
+
+}
+
+
+
+
+// function getApi(requestUrl) {
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       console.log(response);
+//       if (response.status === 200) {
+//         responseText.textContent = response.status;
+//       }
+//       return response.json();
+//   });
+// }
+
+// getApi(requestUrl);
+
+
+
+
+
+
 
 //search by city
 //used api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
 
 function findCurrentCityTemp(){
-var citySearchedURL = "https://api.openweathermap.org/data/2.5/forecast?q="+citySearched+"&appid="+myKey
+
 }
 
 
