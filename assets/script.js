9b554a5e9fb982d36d89c8810086a6f061312aef
Sylvia Ely <sylvianne9417@gmail.com>
2022-11-01 14:57:38 -0700

    figuring out button click

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,30 @@
 //figure out city API
 //input.val() ???
 //search suggestions
+//var citySearched = "input"
+var myKey = "9ee067cb6c60d1cf1d72062b348c22a8"
 
+
+
+
+$("button").on("click", function(event){
+    event.preventDefault();
+    console.log("Hi Sylvia");
+})
+
+//search by city
+//used api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+
+function findCurrentCityTemp(){
+var citySearchedURL = "https://api.openweathermap.org/data/2.5/forecast?q="+citySearched+"&appid="+myKey
+}
+
+
+
+function findFiveDay(){
+//var latVal = //
+//var lonVal = //
+var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+latVal+"&lon="+lonVal+"&appid="+myKey
+
+}
