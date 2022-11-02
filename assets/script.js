cd8cfaf4d02a7bba278896874042d40f6e2da192
Sylvia Ely <sylvianne9417@gmail.com>
2022-11-02 14:49:04 -0700

    pulling current weather information and assigning to variables

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,14 @@
 var myKey = "9ee067cb6c60d1cf1d72062b348c22a8"
 var inputEl = $("input");
 
-
-
-
 $("button").on("click", function(event){
     event.preventDefault();
     var citySearched = inputEl.val();
     verifyCityEntered(citySearched);
 })
 
-
 function verifyCityEntered(citySearched){
-    var citySearchedURL = "https://api.openweathermap.org/data/2.5/forecast?q="+citySearched+"&appid="+myKey
+    var citySearchedURL = "https://api.openweathermap.org/data/2.5/forecast?q="+citySearched+"&appid="+myKey+"&units=imperial";
     console.log(citySearchedURL)
     fetch(citySearchedURL)
         .then(function(response) {
@@ -31,32 +27,32 @@ function verifyCityEntered(citySearched){
         })
         .then(function (data) {
             console.log(data);
-            //printCityInfo(data);
-            findFiveDay(data);
+            printCurrentWeather(data);
+            //findFiveDay(data);
         })
 }
 
 
-
-
-
-
-
-
-//search by city
-//used api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-function printCityInfo(data){
-
+function findFiveDay(data){
+    var latVal = data.city.coord.lat;
+    var lonVal = data.city.coord.lon;
+    console.log(latVal);
+    console.log(lonVal);
+//var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+latVal+"&lon="+lonVal+"&appid="+myKey
 
 }
 
+function printCurrentWeather(data){
+//var currentWeatherEl = $("#currentWeather");
+var currentTemp = data.list[0].main.temp;
+var cityName = data.city.name;
+var currentWind = data.list[0].wind.speed;
+var currentHumidity = data.list[0].main.humidity;
+console.log("temp:"+currentTemp);
+console.log("city: "+cityName);
+console.log("wind: " + currentWind);
+console.log("humidity: " + currentHumidity);
 
 
-function findFiveDay(data){
-var latVal = data.city.coord.lat;
-var lonVal = data.city.coord.lon;
-console.log(latVal);
-console.log(lonVal);
-//var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+latVal+"&lon="+lonVal+"&appid="+myKey
-
+// 
 }
