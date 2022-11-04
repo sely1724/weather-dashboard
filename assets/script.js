a50d319b50d3da8fb6c33434991af0a25f70a28f
Sylvia Ely <sylvianne9417@gmail.com>
2022-11-03 17:28:47 -0700

    adding icon to current temp data.  finalized appending five day information

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,17 @@
 //search suggestions
 //var citySearched = "input"
 var myKey = "9ee067cb6c60d1cf1d72062b348c22a8"
+var time = moment();
+var todaysDate = time.format("MMMM Do, YYYY");
 var inputEl = $("input");
-var cityNameEl = $("#city-name");
+var cityNameEl = $(".city-name");
+var cityFiveDayEl = $("#city-name-five");
 var currentTempEl = $("#current-temp");
 var currentWindEl = $("#current-wind");
 var currentHumidityEl = $("#current-humidity");
+var todaysDateEl = $("#today");
+var weatherIconEl = $("#weather-icon")
+
 
 $("button").on("click", function(event){
     event.preventDefault();
@@ -43,7 +49,6 @@ function findFiveDay(data){
     var latVal = data.city.coord.lat;
     var lonVal = data.city.coord.lon;
     var fiveDayURL = "https://api.openweathermap.org/data/3.0/onecall?lat="+latVal+"&lon="+lonVal+"&appid="+myKey+"&units=imperial";
-    //"https://api.openweathermap.org/data/2.5/forecast?lat="+latVal+"&lon="+lonVal+"&appid="+myKey+"&units=imperial";
     printFiveDayWeather(latVal,lonVal,data,fiveDayURL)
     
 }
@@ -58,22 +63,21 @@ function printFiveDayWeather(latVal,lonVal,data,fiveDayURL){
         return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            //print city name here??
+            console.log(data);            
             for(var i = 1; i < 5; i++){
                 var daysTemp = data.daily[i].temp.day;
                 var daysWind = data.daily[i].wind_speed;
                 var daysHumidity = data.daily[i].humidity;
+                var futureDate = time.add(1, 'days').format("MMMM Do, YYYY");
                 var daysIconText = data.daily[i].weather[0].icon
                 var daysIconImg = "http://openweathermap.org/img/wn/"+daysIconText+"@2x.png";
                 var futureWeatherEl = $(".forecast-block"+i);
-                //create element
-                futureWeatherEl.append("<h3>datehold - Temp</h3>");
-                //add image here???
-                futureWeatherEl.append("<p>"+daysTemp+"</p>");
+                //create five day elements
+                futureWeatherEl.append("<h4>"+futureDate+"</h4>");
                 futureWeatherEl.append("<img src = "+daysIconImg+"></img");
-                futureWeatherEl.append("<p>"+daysWind+"</p>");
-                futureWeatherEl.append("<p>"+daysHumidity+"</p>");
+                futureWeatherEl.append("<p> Temperature: "+daysTemp+" degrees</p>");
+                futureWeatherEl.append("<p>Wind Speed: "+daysWind+"mph</p>");
+                futureWeatherEl.append("<p>Humidity: "+daysHumidity+"%</p>");
             }
 
 
@@ -81,17 +85,9 @@ function printFiveDayWeather(latVal,lonVal,data,fiveDayURL){
         })
 
 //NEXT STEPS:
-//1. fetch temperature image.
-//2. fetch date with moment.
+//1. fetch date with moment.
 
 }
-
-
-
-
-
-
-
 
 
 
@@ -109,8 +105,17 @@ function printCurrentWeather(data){
     var cityName = data.city.name;
     var currentWind = data.list[0].wind.speed;
     var currentHumidity = data.list[0].main.humidity;
+
+
+
+    var daysIconText = data.list[0].weather[0].icon;
+    var daysIconImg = "http://openweathermap.org/img/wn/"+daysIconText+"@2x.png";
     cityNameEl.text(cityName);
-    currentTempEl.text("Temperature: "+currentTemp); 
-    currentWindEl.text("Wind: "+currentWind +"mph");
+    cityFiveDayEl.text(cityName+"'s Five Day Forecast")
+    console.log(todaysDate)
+    todaysDateEl.text(todaysDate);
+    weatherIconEl.append("<img src = "+daysIconImg+"></img");
+    currentTempEl.text("Temperature: "+currentTemp+" degrees"); 
+    currentWindEl.text("Wind Speed: "+currentWind +"mph");
     currentHumidityEl.text("Humidity: "+currentHumidity+"%");
 }
